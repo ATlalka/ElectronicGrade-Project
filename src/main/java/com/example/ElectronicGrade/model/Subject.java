400c0f3bb121d8404f4654d32531fa7a6700b590
Ola Tlałka <otlalka@gmail.com>
2021-12-19 08:46:50 +0100

    Subject added

---
+++ src/main/java/com/example/ElectronicGrade/model/Subject.java
@@ -0,0 +1,18 @@
+package com.example.ElectronicGrade.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "Przedmioty")
+public class Subject {
+
+    @Id
+    @GeneratedValue
+    @Column (name = "idPrzedmiot")
+    private Long id;
+
+    @Column (name = "nazwa")
+    private String name;
+
+    // TO DO: przedmioty z zajęciami
+}
