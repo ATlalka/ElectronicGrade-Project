df282fc2caf0623b68004265a424a8e0c9a6b750
Ola Tlałka <65178593+ATlalka@users.noreply.github.com>
2021-12-21 11:05:28 +0100

    Poprawa kluczy nauczyciela

--- sql/dml/7_Klasy.sql
+++ sql/dml/7_Klasy.sql
@@ -1,1 +1,1 @@
-INSERT INTO Klasy (`idKlasa`, `NauczycieleidNauczyciel`, `Rok`, `Symbol`, `Rocznik`) VALUES ("1", "1", "2", "A", "2020"),("2", "2", "3", "B", "2019")
+INSERT INTO Klasy (`idKlasa`, `NauczycieleidNauczyciel`, `Rok`, `Symbol`, `Rocznik`) VALUES ("1", "13", "2", "A", "2020"),("2", "14", "3", "B", "2019");
