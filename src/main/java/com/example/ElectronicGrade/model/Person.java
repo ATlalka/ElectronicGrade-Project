f2257551d1847f4404b34bc83fd8338dd874d1ad
mikolachmielecki <mikolajchmielecki2000@gmail.com>
2021-12-21 18:56:51 +0100

    osoby

---
+++ src/main/java/com/example/ElectronicGrade/model/Person.java
@@ -0,0 +1,27 @@
+package com.example.ElectronicGrade.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Osoby")
+public class Person {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "idOsoba")
+    private Long id;
+
+    @Column(name = "imie")
+    private String name;
+
+    @Column(name = "nazwisko")
+    private String surname;
+
+    @Column(name = "pesel")
+    private Long code;
+
+    @Column(name = "dataUrodzenia")
+    private Date birthdayDate;
+    
+}
