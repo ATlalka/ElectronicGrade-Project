909f243a973e48fd427c0c44f9fc204949a41f94
Nat3005 <natalia.rusin.bb@gmail.com>
2022-01-11 23:51:26 +0100

    designer files bo coś sie rozjechało ale one chyba nie sa potrzebne

---
+++ frontend/my-view.ts
@@ -0,0 +1,25 @@
+import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+
+@customElement('my-view')
+export class MyView extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+          display: block;
+          height: 100%;
+      }
+      `;
+  }
+
+  render() {
+    return html`
+<vaadin-horizontal-layout theme="spacing" style="width: 100%; height: 100%;"></vaadin-horizontal-layout>
+`;
+  }
+
+  // Remove this method to render the contents of this view inside Shadow DOM
+  createRenderRoot() {
+    return this;
+  }
+}
